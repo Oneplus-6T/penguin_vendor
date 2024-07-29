cb3e46484269a2265190cba409b74747188d8d69
Akshat Singh <akshatleo9456@gmail.com>
2024-07-29 13:55:54 +0000

    products: Update beryllium to uvite

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -65,7 +65,7 @@ PRODUCT_MAKEFILES += \
 COMMON_LUNCH_CHOICES += \
     aospa_alioth-userdebug \
     aospa_apollo-userdebug \
-    aospa_beryllium-userdebug \
+    aospa_beryllium-user \
     aospa_bitra-userdebug \
     aospa_bladerunner-userdebug \
     aospa_bluejay-userdebug \
